d099585352b222ff0c239eca647b1d9550d62513
Julien Kipp <julien@doji-tech.com>
2024-09-08 13:54:19 +0200

    work on API for MobileSAM predictor implement Predict() method taking point coordinates as inputs

--- com.doji.mobilesam/Runtime/Scripts/MobileSAM.cs
+++ com.doji.mobilesam/Runtime/Scripts/MobileSAM.cs
@@ -4,40 +4,50 @@ using UnityEngine;
 
 namespace Doji.AI.Segmentation {
 
+    public struct DecoderOutput {
+        public Tensor<float> LowResMasks { get; private set; }
+        public Tensor<float> IoUPredictions { get; private set; }
+        public Tensor<float> Masks { get; private set; }
+
+        public DecoderOutput(Tensor lowResMasks, Tensor iouPredictions, Tensor masks) {
+            LowResMasks = lowResMasks as Tensor<float>;
+            IoUPredictions = iouPredictions as Tensor<float>;
+            Masks = masks as Tensor<float>;
+        }
+    }
+
+    /// <summary>
+    /// Predictor using MobileSAM models.
+    /// </summary>
     public class MobileSAM : IDisposable {
 
-        public static Model Encoder {
+        public Model Encoder {
             get {
                 if (_encoderAsset == null) {
                     _encoderAsset = Resources.Load<ModelAsset>("ONNX/mobilesam.encoder");
                 }
                 if (_encoderAsset == null) {
                     Debug.LogError("MobileSAM encoder ONNX model not found.");
-                } else if (_encoderModel == null) {
-                    _encoderModel = ModelLoader.Load(_encoderAsset);
                 }
-                return _encoderModel;
+                return _encoderModel ??= ModelLoader.Load(_encoderAsset);
             }
         }
-        private static Model _encoderModel;
+        private Model _encoderModel;
         private static ModelAsset _encoderAsset;
 
-        public static Model Decoder {
+        public Model Decoder {
             get {
                 if (_decoderAsset == null) {
                     _decoderAsset = Resources.Load<ModelAsset>("ONNX/mobilesam.decoder");
                 }
                 if (_decoderAsset == null) {
                     Debug.LogError("MobileSAM decoder ONNX model not found.");
-                } else if (_decoderModel == null) {
-                    _decoderModel = ModelLoader.Load(_decoderAsset);
                 }
-                return _decoderModel;
+                return _decoderModel ??= ModelLoader.Load(_decoderAsset);
             }
         }
         private static Model _decoderModel;
-        private static ModelAsset _decoderAsset;
-
+        private ModelAsset _decoderAsset;
 
         /// <summary>
         /// Which <see cref="BackendType"/> to run the model with.
@@ -69,10 +79,12 @@ namespace Doji.AI.Segmentation {
         private Worker _decoder;
         private Tensor[] _inputTensors = new Tensor[6];
 
-        /// <summary>
-        /// the (possibly resized) input texture;
-        /// </summary>
-        public RenderTexture _resizedInput;
+        private bool _isImageSet;
+        private Vector2Int _origSize;
+        private Vector2Int _inputSize;
+        private Tensor _features;
+
+        public RenderTexture Result { get; private set; }
 
         /// <summary>
         /// Initializes a new instance of MiDaS.
@@ -80,6 +92,7 @@ namespace Doji.AI.Segmentation {
         /// <param name="model">the reference to a MiDaS ONNX model</param>
         public MobileSAM() {
             InitializeNetwork();
+            Result = new RenderTexture(1024, 1024, 0, RenderTextureFormat.RFloat);
         }
 
         private void InitializeNetwork() {
@@ -90,100 +103,143 @@ namespace Doji.AI.Segmentation {
                 return;
             }
 
-            var graph = new FunctionalGraph();
-            var inputs = graph.AddInputs(Encoder);
-            inputs[0] *= 255f;
-            FunctionalTensor[] outputs = Functional.Forward(Encoder, inputs);
-            _encoderModel = graph.Compile(outputs);
+            _encoderModel = AddPreprocessing(_encoderModel);
 
             _encoder = new Worker(Encoder, Backend);
             _decoder = new Worker(Decoder, Backend);
-            _resizedInput = new RenderTexture(1024, 1024, 0);
-
-            Debug.Log(_encoderModel.inputs.Count);
-            Debug.Log(_encoderModel.inputs[0].name);
-            Debug.Log(_encoderModel.inputs[0].shape);
-        }
-
-        public void PredictMasks(Texture input) {
-            Tensor embeddings = EncodeImage(input);
-            DecodeMasks(input, embeddings);
         }
 
         /// <summary>
-        /// Encodes the input image and outputs an image embedding as a Tensor.
+        /// Encodes the given image and predicts the masks with the given parameters.
+        /// TOOD: for better performance expose methods that allow to encode image once,
+        /// and predict masks multiple times.
         /// </summary>
-        private Tensor EncodeImage(Texture input) {
-            using Tensor inputTensor = TextureConverter.ToTensor(input, input.width, input.height, 3);
-            inputTensor.Reshape(new TensorShape(input.height, input.width, 3));
-            _encoder.Schedule(inputTensor);
-            Tensor embeddings = _encoder.PeekOutput();
-            return embeddings;
+        public void PredictMasks(
+            Texture input,
+            float[] pointCoords = null,
+            float[] pointLabels = null,
+            Rect? box = null,
+            Texture maskInput = null)
+        {
+            SetImage(input);
+            Predict(pointCoords, pointLabels, box, maskInput);
         }
 
         /// <summary>
-        /// Predict masks for the given input prompts, using the given <paramref name="imageEmbeddings"/>s.
+        /// Encodes the input image and stores the calculated image embeddings, allowing
+        /// masks to be predicted with the <see cref="PredictMasks"/> method.
         /// </summary>
-        /// <param name="imageEmbeddings"></param>
+        private void SetImage(Texture image) {
+            Vector2Int origSize = new Vector2Int(image.height, image.width);
+            // Transform the image to the form expected by the model
+            //input_image = self.transform.apply_image(image)
+            TextureTransform transform = new TextureTransform();
+            transform.SetTensorLayout(TensorLayout.NHWC);
+            using Tensor inputImageTensor = TextureConverter.ToTensor(image, transform);
+            inputImageTensor.Reshape(inputImageTensor.shape.Squeeze(0));
+            SetImage(inputImageTensor, origSize);
+        }
+
+        private void SetImage(Tensor transformedImage, Vector2Int origSize) {
+            //Debug.Assert(Math.Max(input.width, input.height) == 1024, "set_torch_image input image must have a long sideof 1024.");
+            ResetImage();
+            _origSize = origSize;
+            _inputSize = new Vector2Int(transformedImage.shape[-2], transformedImage.shape[-1]);
+            // preprocessing/normalization is already baked into the model
+            _encoder.Schedule(transformedImage);
+            _features = _encoder.PeekOutput();
+            _isImageSet = true;
+        }
+
+        /// <summary>
+        /// Resets the currently set image.
+        /// </summary>
+        private void ResetImage() {
+            _isImageSet = false;
+            _features = null;
+            _origSize = default;
+            _inputSize = default;
+        }
+
+        /// <summary>
+        /// Predict masks for the given input prompts, using the currently set image.
+        /// </summary>
         /// <param name="pointCoords">A Vector2 array of point prompts to the model.
-        /// Each point is in (X, Y) in pixels.</param>
+        /// Each point is given in pixel coordinates.</param>
         /// <param name="pointLabels">A length N array of labels for the point prompts.
         /// 1 indicates a foreground point and 0 indicates a background point.</param>
         /// <param name="box">a box prompt to the model.</param>
         /// <param name="maskInput">A low resolution mask input to the model, typically
         /// coming from a previous prediction iteration.</param>
-        /// <param name="multimask_output">If true, the model will return three masks.
-        /// For ambiguous input prompts(such as a single click), this will often
-        /// produce better masks than a single prediction.If only a single
-        /// mask is needed, the model's predicted quality score can be used
-        /// to select the best mask.For non-ambiguous prompts, such as multiple
-        /// input prompts, multimask_output=False can give better results.</param>
-        /// <param name="returnLogits">If true, returns un-thresholded masks logits
-        /// instead of a binary mask.</param>
-        private void DecodeMasks(
-            Texture inputImage,
-            Tensor imageEmbeddings,
+        public void Predict(
             float[] pointCoords = null,
             float[] pointLabels = null,
             Rect? box = null,
-            Texture maskInput = null,
-            bool multimaskOutput = true,
-            bool returnLogits = false
-        ) {
-            pointCoords = new float[] { inputImage.width / 2f, inputImage.height / 2f };
-            pointLabels = new float[] { 1f };
+            Texture maskInput = null)
+        {
+            if (!_isImageSet) {
+                throw new InvalidOperationException("An image must be set with .SetImage(...) before mask prediction.");
+            }
+            if (box != null) {
+                throw new NotImplementedException("Box inputs not supported yet.");
+            }
+            if (maskInput != null) {
+                throw new NotImplementedException("Mask inputs not supported yet.");
+            }
 
+            if (pointCoords != null) {
+                Debug.Assert(pointLabels != null, "pointLabels must be supplied if point_coords is supplied.");
+            }
             int numPoints = pointCoords?.Length / 2 ?? 0;
             int numLabels = pointLabels?.Length ?? 0;
             if (numPoints != numLabels) {
                 throw new ArgumentException("number of point labels does not match the number of points.");
             }
-
             using Tensor<float> point_coords = new Tensor<float>(new TensorShape(1, numPoints, 2), pointCoords);
             using Tensor<float> point_labels = new Tensor<float>(new TensorShape(1, numPoints), pointLabels);
             using Tensor<float> mask_input = new Tensor<float>(new TensorShape(1, 1, 256, 256));
             using Tensor<float> has_mask_input = new Tensor<float>(new TensorShape(1), new float[] { 0f });
-            using Tensor<float> orig_im_size = new Tensor<float>(new TensorShape(2), new float[] { inputImage.width, inputImage.height });
+            using Tensor<float> orig_im_size = new Tensor<float>(new TensorShape(2), new float[] { _origSize.x, _origSize.y });
 
-            _inputTensors[0] = imageEmbeddings; // image_embeddings
-            _inputTensors[1] = point_coords;    // point_coords    
-            _inputTensors[2] = point_labels;    // point_labels    
-            _inputTensors[3] = mask_input;      // mask_input      
-            _inputTensors[4] = has_mask_input;  // has_mask_input  
-            _inputTensors[5] = orig_im_size;    // orig_im_size    
+            var result = Predict(point_coords, point_labels, mask_input, has_mask_input, orig_im_size);
+
+            TextureConverter.RenderToTexture(result.Masks, Result);
+        }
+
+        private DecoderOutput Predict(
+            Tensor pointCoords,
+            Tensor pointLabels,
+            Tensor maskInput,
+            Tensor hasMaskInput,
+            Tensor origImSize)
+        {
+            _inputTensors[0] = _features;       // image_embeddings
+            _inputTensors[1] = pointCoords;     // point_coords    
+            _inputTensors[2] = pointLabels;     // point_labels    
+            _inputTensors[3] = maskInput;       // mask_input      
+            _inputTensors[4] = hasMaskInput;    // has_mask_input  
+            _inputTensors[5] = origImSize;      // orig_im_size    
 
             _decoder.Schedule(_inputTensors);
-            Tensor masks = _decoder.PeekOutput("masks");
-            Debug.Log("Mask output shape " + masks.shape);
-            int height = masks.shape[2];
-            int width = masks.shape[3];
+            var lowResMasks = _decoder.PeekOutput("low_res_masks");
+            var iouPredictions = _decoder.PeekOutput("iou_predictions");
+            var masks = _decoder.PeekOutput("masks");
 
-            Result = TextureConverter.ToTexture(masks as Tensor<float>, width, height);
-
+            return new DecoderOutput(lowResMasks, iouPredictions, masks);
         }
-        
-        public Texture Result { get; private set; }
-        
+
+        /// <summary>
+        /// Normalize pixel values and pad to a square input.
+        /// </summary>
+        private Model AddPreprocessing(Model model) {
+            var graph = new FunctionalGraph();
+            var inputs = graph.AddInputs(model);
+            // [0, 1] -> [0, 255]
+            inputs[0] *= 255f;
+            FunctionalTensor[] outputs = Functional.Forward(model, inputs);
+            return graph.Compile(outputs);
+        }
+
         public void Dispose() {
             _encoder?.Dispose();
             _decoder?.Dispose();
--- com.doji.mobilesam/Samples~/01-BasicSample/BasicSample.cs
+++ com.doji.mobilesam/Samples~/01-BasicSample/BasicSample.cs
@@ -4,46 +4,72 @@ using UnityEngine.UI;
 
 namespace Doji.AI.Segmentation.Samples {
 
-    public class BasicSample : MonoBehaviour, IPointerClickHandler {
+    public class MobileSAM_BasicSample : MonoBehaviour, IPointerClickHandler {
 
         private MobileSAM _mobileSAMPredictor;
 
         public RawImage SourceImage;
+        public RawImage MaskImage;
         public RectTransform DebugMarker;
+        public RenderTexture Result;
 
+        private Vector2? _currentPoint;
+        private Camera _eventCamera;
 
         public void Start () {
             _mobileSAMPredictor = new MobileSAM();
+            Result = _mobileSAMPredictor.Result;
+            MaskImage.texture = _mobileSAMPredictor.Result;
         }
 
         private void OnDestroy() {
             _mobileSAMPredictor?.Dispose();
         }
 
-        public void PredictMask() {
-            _mobileSAMPredictor.PredictMasks(SourceImage.texture);
+        public void PredictMask(Vector2 point) {
+            //point *= new Vector2(1f, -1f);
+            //Debug.Log(point);
+            var points = new float[] { point.x, point.y };
+            var labels = new float[] { 1f /* foreground */ };
+            _mobileSAMPredictor.PredictMasks(SourceImage.texture, points, labels);
+            MaskImage.enabled = true;
         }
       
         public void OnPointerClick(PointerEventData eventData) {
             if (eventData.button == PointerEventData.InputButton.Left) {
                 RectTransform rectTransform = SourceImage.GetComponent<RectTransform>();
+                _currentPoint = eventData.position;
+                _eventCamera = eventData.pressEventCamera;
 
                 Vector2 localPoint;
                 RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, eventData.position, eventData.pressEventCamera, out localPoint);
-                
+
                 Rect rect = rectTransform.rect;
                 float normalizedX = (localPoint.x - rect.x) / rect.width;
                 float normalizedY = (localPoint.y - rect.y) / rect.height;
 
-                Vector2 uvPosition = new Vector2(normalizedX, normalizedY);
+                Vector2 uvPosition = new Vector2(normalizedX, 1.0f - normalizedY);
+                Vector2 textureSpacePos = new Vector2(SourceImage.texture.width, SourceImage.texture.height) * uvPosition;
+                Debug.Log(textureSpacePos);
 
                 // Ensure click was inside of the texture area
                 if (uvPosition.x >= 0 && uvPosition.x <= 1 && uvPosition.y >= 0 && uvPosition.y <= 1) {
                     MoveDebugMarker(localPoint);
-                    PredictMask();
+                    PredictMask(textureSpacePos);
                 }
             }
         }
+
+        private void OnRectTransformDimensionsChange() {
+            // move point in case canvas changed size
+            if (_currentPoint != null) {
+                RectTransform rectTransform = SourceImage.GetComponent<RectTransform>();
+                Vector2 localPoint;
+                RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, _currentPoint.Value, _eventCamera, out localPoint);
+                MoveDebugMarker(localPoint);
+            }
+        }
+
         private void MoveDebugMarker(Vector2 localPosition) {
             if (DebugMarker != null) {
                 DebugMarker.anchoredPosition = localPosition;
