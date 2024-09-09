f7e58f7b7c575b70f04a75b6b1f654e3a1c19fe6
Julien Kipp <julien@doji-tech.com>
2024-09-09 13:02:23 +0200

    add AssemblyInfo

---
+++ com.doji.mobilesam/Runtime/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("Doji.MobileSAM")]
+[assembly: AssemblyDescription("Fast Segmentation with Unity Sentis using MobileSAM")]
+[assembly: AssemblyCompany("Doji Technologies")]
+[assembly: AssemblyProduct("Doji.MobileSAM")]
+[assembly: AssemblyCopyright("Copyright © Julien Kipp 2024")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
